0c023f05b182b451fa539408538359b2f71eebba
nestor <nestorivanmo@comunidad.unam.mx>
2020-10-02 13:33:13 -0500

    ...

---
+++ pase/montecarlo.py
@@ -0,0 +1,11 @@
+import numpy as np
+def G(x):
+  return np.exp(-x**2)
+
+def monte_carlo(G, a, b, M):
+  s = 0
+  for i in range(M):
+    s += G(a + (b - a) * np.random.uniform(0, 1, 1))
+  return ((b - a)/M) * s[0]
+
+print(monte_carlo(G, 1, 3, 1000))
