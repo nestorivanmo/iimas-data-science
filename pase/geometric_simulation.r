9b8be351ee8c1430d1951f7d9246c93eea1c4743
nestor <nestorivanmo@comunidad.unam.mx>
2020-10-29 11:33:31 -0600

    ...

--- pase/geometric_simulation.r
+++ pase/geometric_simulation.r
@@ -1,12 +1,14 @@
-#---------------------
-# Simulación de una VA
-#---------------------
-f <- function(x, n = 1) {
+# SIMULACIÓN DE LA VARIABLE ALEATORIA GEOMÉTRICA
+
+#--------------------------
+# Primer método
+#--------------------------
+f <- function(p, n = 1) {
     vec <- c()
     for(i in 1:n) {
         num_iter <- 0
         while(T){
-            if(runif(1, 0, 1) < p){
+            if(runif(1) < p){
                 vec <- c(vec, num_iter)
                 break
             }
@@ -15,51 +17,53 @@ f <- function(x, n = 1) {
     }   
     return(vec)
 }
+hist(f(.09, n=10000))
 
-#----------------------
-# Simulación geométrica
-#----------------------
-# P(X=x) = p(1-p)^(x-1)
-p <- function(p, x) {
-  return (p*((1-p)**(x-1)))
-}
-r <- p(10/12, seq(1, 12))
-r
-
-
-#---------------------
-# Código para graficar
-#---------------------
-graph <- function(n, p) {
-  valores_aleatorios <- f(p, n)
-  cuenta <- table(valores_aleatorios)
-  plot(cuenta)  
-}
-graph(100, 0.4)
-
-densidad <- function(x, p){
-  if(x == -1){
-    return(0);
-  }
+#--------------------------
+# Segundo método: Transformanda inversa
+#--------------------------
+geom <- function(x, p){
   return(p * (1 - p) ^ x);
 }
 
-P <- function(p){
-  x <- runif(1);
-  # Vamos a truncar los intervalos a este numero maximo
-  maximo <- 10 %/% p;  
-  #print(maximo);
-  #print(x)
-  limite_superior <- 0
-  for(i in 0:maximo){
-    limite_inferior <- limite_superior
-    limite_superior <- limite_superior + densidad(i, p)
-    if(x > limite_inferior && x < limite_superior){
-      return(i);
-    }
-  } 
-  return(maximo);
+geom_inverse <- function(p, y) {
+  numerador <- log(1/p) + log(1-y)
+  denominador <- log(1-p)
+  return (numerador / denominador)
+}
+v <- runif(1000)
+hist(geom_inverse(0.09, v))
+
+
+
+#--------------------------
+# Tercer método: Intervalos
+#--------------------------
+densidad <- function(x, p){
+  return(p * (1 - p) ^ x);
 }
 
-print(P(.08))
+P <- function(p, n = 1){
+  v <- c()
+  for(i in 1:n){
+    x <- runif(1);
+    # Vamos a truncar los intervalos a este numero maximo
+    maximo <- 10 %/% p; 
+    limite_superior <- 0
+    for(i in 0:maximo){
+      limite_inferior <- limite_superior;
+      limite_superior <- limite_superior + densidad(i, p);       
+      if(limite_inferior < x && x < limite_superior){
+        v <- c(v, i);
+        break;
+      }
+    } 
+    if(x > limite_superior){
+      v <- c(v, maximo + 1);
+    }
+  }
+  return(v);
+}
+hist(P(.07, n=10000))
+
 
