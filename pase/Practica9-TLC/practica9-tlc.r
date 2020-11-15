4e95a888f5b2f9689e3b00e841e00d52b67f6a16
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-11-15 17:38:28 -0600

    ...

--- pase/Practica9-TLC/practica9-tlc.r
+++ pase/Practica9-TLC/practica9-tlc.r
@@ -1,5 +1,6 @@
 Z <- function(n, p, k) {
   bin <- rbinom(n, k, p)
+  print(bin)
   mu_k <- k*n*p
   sigma_k <- sqrt(k*n*p*(1-p))
   z <- (sum(bin) - mu_k) / sigma_k
@@ -11,11 +12,23 @@ bin_vs_norm <- function(N, n, p, k) {
   for (i in 1:N) {
     Z_vec <- c(Z_vec, Z(n, p, k))
   }
-  hist(Z_vec, probability = TRUE, xlab = 'Z', ylab = 'Z',
-       main = paste('N(0,1) - Bin(',n,',',p,')\n k =', k));
+  hist(Z_vec, probability = TRUE, xlab = 'X', ylab = 'Z',
+       main = paste('N(0,1) ~ Bin(',n,',',p,')\n k =', k));
   sop <- seq(-6, 6, length.out=1000)
   par(new = TRUE)
   lines(sop, dnorm(sop))
 }
 
-bin_vs_norm(10000, 10, 0.5, 10)
+N = 10000
+bin_vs_norm(N,10, 0.5, 10)
+bin_vs_norm(N,10, 0.5, 20)
+bin_vs_norm(N,10, 0.5, 100)
+bin_vs_norm(N,10, 0.5, 1000)
+bin_vs_norm(N,10, 0.99, 10)
+bin_vs_norm(N,10, 0.99, 20)
+bin_vs_norm(N,10, 0.99, 100)
+bin_vs_norm(N,10, 0.99, 1000)
+
+Z(1000, 0.5, 10)
+Z(1000, 0.99, 10)
+
