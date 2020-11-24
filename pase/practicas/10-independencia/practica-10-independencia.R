0fb77c76fb77711c0a0530a7293233a095aa51ed
nestor <nestorivanmo@comunidad.unam.mx>
2020-11-24 09:48:37 -0600

    ...

--- pase/practicas/10-independencia/practica-10-independencia.R
+++ pase/practicas/10-independencia/practica-10-independencia.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(RColorBrewer)
 
+
 #Ejercicio 1: Normal
 
 #1000 puntos (X,Y)
@@ -12,7 +13,8 @@ y <- rnorm(n, p)
 df <- data.frame(x=x, y=y)
 ggplot(data = df) + 
   geom_jitter(mapping = aes(x = x, y = y), color='darkblue', shape=1, size=2.5) +
-  labs(x='X', y='Y', title=paste('Parejas (X,Y) de una distribución normal con', n, ' puntos')) + 
+  labs(x='X', y='Y', title=paste('Parejas (X,Y) de una distribución normal con', 
+                                 n, ' puntos')) + 
   theme_bw()
 
 #1000 puntos (Z,W)
@@ -25,17 +27,43 @@ W <- X+Y
 df <- data.frame(x = Z, y = W)
 ggplot(data = df) + 
   geom_jitter(mapping = aes(x = Z, y = W), color = 'darkred', shape=1, size=2.5) + 
-  labs(x='Z = X - Y', y = 'W = X + Y', title=paste('Parejas (Z,W) de una distribución normal con', n, ' puntos')) +
+  labs(x='Z = X - Y', y = 'W = X + Y', title=paste('Parejas (Z,W) de una distribución 
+                                                   normal con', n, ' puntos')) +
   theme_bw()
 
 
 #Ejercicio 2: Multinomial
+generate_X <- function(n, verbose=FALSE) {
+  X1 <- 0
+  X2 <- 0
+  X3 <- 0
+  B <- runif(n, min=1, max=300)
+  for (x in B) {
+    if (x <= 100) {
+      X1 <- X1 + 1 
+    } else if (x > 100 && x <= 200) {
+      X2 <- X2 + 1
+    } else {
+      X3 <- X3 + 1
+    }
+  }
+  X <- c(X1, X2, X3)
+  if (verbose) {
+    print(X1)
+    print(X2)
+    print(X3)
+  }
+  return (X)
+}
 
-p <- 1/3
+graph_X <- function(n) {
+  X <- generate_X(n)
+  X1 <- X[1]
+  X2 <- X[2]
+  X3 <- X[3]
+  df <- data.frame(x=X1, y=X2, z=X3)
+  ggplot(data = df) + 
+    geom_raster(mapping = aes(fill = X3), x=X1, y=X2)
+}
 
-
-
-
-
-
-
+graph_X(100)
