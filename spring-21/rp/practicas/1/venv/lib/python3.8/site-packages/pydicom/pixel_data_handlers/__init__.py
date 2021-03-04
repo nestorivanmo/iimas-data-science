15d3a561bc9b556989bc910677e9e62b6f42d2c1
nestorivanmo <nestor.martinez.98@hotmail.com>
2021-03-04 08:55:05 -0600

    removed unnecesary files

--- spring-21/rp/practicas/1/venv/lib/python3.8/site-packages/pydicom/pixel_data_handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from pydicom.pixel_data_handlers.util import (
-    apply_color_lut, apply_modality_lut, apply_voi_lut, convert_color_space,
-    apply_voi, apply_windowing
-)
-
-apply_rescale = apply_modality_lut
