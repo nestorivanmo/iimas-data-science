5621d5d117ddbf0becf82d30ef1c13dfc33c55d1
nestorivanmo <nestor.martinez.98@hotmail.com>
2021-04-20 16:37:37 -0500

    ...

--- spring-21/bne/practicas/practica_redis/add_to_wishlist.lua
+++ spring-21/bne/practicas/practica_redis/add_to_wishlist.lua
@@ -1,5 +1,24 @@
 local user_id = KEYS[1]
+local url_seq_key = KEYS[2]
+local rurl_seq_key = KEYS[3]
+local base_client_key = KEYS[4]
+local base_client = redis.call('get', base_client_key)
+
 local url_to_add = ARGV[1]
+local category = ARGV[2]
+local public = ARGV[3]
+local private = ARGV[4]
+
 local wishlist_key = 'wishlist:'..user_id
-redis.call('rpush', wishlist_key, url_to_add)
-redis.call('lrange', wishlist_key, 0, -1)
+local new_url_key = redis.call('incr', url_seq_key)
+local rurl_new_key = redis.call('incrby', rurl_seq_key, 25)
+local rurl = base_client..rurl_new_key
+local key = 'url:'..new_url_key..':'..user_id
+
+redis.call('hset', key, 'url', url_to_add, 'rurl', rurl, 'public', public, 'private', private, 'category', category, 'wishlist', '1')
+redis.call('rpush', wishlist_key, new_url_key)
+
+local categories_key = 'categories:'..user_id
+redis.call('sadd', categories_key, category)
+
+return redis.call('lrange', wishlist_key, 0, -1)
