f93b12522469d1194caf3086b0661473f985a7cf
nestorivanmo <nestor.martinez.98@hotmail.com>
2021-10-21 16:15:41 -0500

    ...

---
+++ fall-21/analisis-multivariado/primer-examen/npcacar.R
@@ -0,0 +1,45 @@
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+
+
+
+#read data
+xdat = read.table("cars.dat")
+
+#delete first column
+x1 = xdat[, -1]
+brand = xdat[,1]
+x2 = x1
+
+#define variable names
+colnames(x2) = c("economy", "service", "value", "price", "design", "sporty",
+  "safety", "easy")
+
+### Standardize Data
+x3  = scale(x1)
+ # calculates eigenvalues and eigenvectors and sorts them by size
+e  = eigen(cor(x3))
+
+#calculates eigenvalues and eigenvectors and sorts them by size
+e1<-e$values
+
+vecs <- e$vectors[,1:2] 
+print(vecs)
+
+#data multiplied by eigenvectors
+x<-as.matrix(x3)%*%e$vectors 
+
+#par(mfrow=c(2,2))
+#plot of the first vs. second PC
+plot(x[,1],x[,2],type="n",xlab="PC1",ylab="PC2",main="Normalized First vs. Second PC",cex.lab=1.2,cex.axis=1.2,cex.main=1.8)
+
+
+#plot(q, type = "n", xlab = "First Factor", ylab = "Second Factor",
+#  main = "Car Marks Data", xlim = c(-1.8, 1), ylim = c(-0.7, 0.7),
+ #    cex.lab = 1.4, cex.axis = 1.4, cex.main = 1.8)
+text(x[,1],x[,2], brand, cex = 1.2, xpd = NA)
+#abline(v = 0)
+#abline(h = 0)
---
+++ fall-21/analisis-multivariado/primer-examen/npcacarsi.R
@@ -0,0 +1,37 @@
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+
+#read data
+xdat = read.table("cars.dat")
+
+#delete first column
+x1 = xdat[, -1]
+
+# non standardized eigen
+ee <- eigen(cov(x1))
+ee1 <-  ee$values
+
+ # standardizes the data matrix
+x  = scale(x1)
+n  = nrow(x)
+ # calculates eigenvalues and eigenvectors and sorts them by size
+e  = eigen(cor(x))
+e1 = e$values
+vec = e$vectors
+ele <- diag(sqrt(e1),ncol(x))
+CORR  <- vec%*%ele
+q <- CORR[,1:2]
+
+ # plot for the relative proportion of variance explained by PCs
+dev.new()
+plot(ee1,xlab="Index",ylab="Lambda",main="cars",cex.lab=1.2,cex.axis=1.2,cex.main=1.8)
+
+ # plot for the correlations of the original variables with the PCs
+dev.new()
+ucircle = cbind(cos((0:360)/180*pi),sin((0:360)/180*pi))
+plot(ucircle,type="l",lty="solid",col="blue",xlab="First PC",ylab="Second PC",main="Car marks data",cex.lab=1.2,cex.axis=1.2,cex.main=1.8,lwd=2)
+abline(h=0.0,v=0.0)
+label   = c("economy", "service", "value", "price", "design", "sporty",
+  "safety", "easy")
+text(q,label,cex=1.2)
